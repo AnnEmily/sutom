45b4451dcad79a15134b4b67b2c5dfc497052d26
Ann Emily Grégoire <annemily.gregoire@sheertex.com>
2025-12-23 17:11:02 -0500

    Retouch indexes

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import Solver from "./Solver";
 
-const styleLink = document.createElement("link");
-styleLink.rel = "stylesheet";
-styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
-document.head.appendChild(styleLink);
+// const styleLink = document.createElement("link");
+// styleLink.rel = "stylesheet";
+// styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+// document.head.appendChild(styleLink);
 
 ReactDOM.render(
   <React.StrictMode>
