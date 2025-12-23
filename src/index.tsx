3a9183faf9364f8d0ee7ce3025a95137a9051c5a
Ann Emily Grégoire <annemily.gregoire@sheertex.com>
2025-12-23 16:45:07 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import * as ReactDOMClient from "react-dom/client";
-// import { Container } from "semantic-ui-react";
-
+import ReactDOM from "react-dom";
 import Solver from "./Solver";
-
-const rootElement = document.getElementById("root");
-const root = ReactDOMClient.createRoot(rootElement);
 
 const styleLink = document.createElement("link");
 styleLink.rel = "stylesheet";
 styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
 document.head.appendChild(styleLink);
 
-// root.render(
-//   <Container>
-//     <Solver />
-//   </Container>
-// );
-
-root.render(
-  <Solver />
+ReactDOM.render(
+  <React.StrictMode>
+    <Solver />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
